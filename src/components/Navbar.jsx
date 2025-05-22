06606afbb002dc88411966109ae6bb4e9ad27f95
Error404 <apupaul35@gmail.com>
2025-05-22 15:03:15 +0600

    Set up the routing

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2>This Navbar</h2>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/routes/router.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router';
+import Root from '../layouts/Root';
+import Home from '../components/Home';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        Component: Root,
+        children: [
+            {
+                index: true,
+                Component: Home
+            },
+        {
+
+        }
+        ]
+    }
+])
+export default router;
